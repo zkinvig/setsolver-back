cbaaa5201d7f68d0131b756a673f34df680a9b46
Zoe Kinvig <zkinvig@uvic.ca>
2025-06-24 11:55:25 -0700

    Initial spring boot setup

---
+++ src/main/java/com/zkinvig/setsolver/SetSolverApplication.java
@@ -0,0 +1,13 @@
+package com.zkinvig.setsolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetSolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetSolverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zkinvig/setsolver/SetSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zkinvig.setsolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetSolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
